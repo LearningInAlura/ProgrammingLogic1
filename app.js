2432dbf5c602c5f3db8cfdd5c08586a33937ab7a
unknown <thiagofrancisco627@gmail.com>
2023-11-24 00:15:40 -0300

    Realizado a implementação de um if ternario - adicionado o certigicado de finalização deste curso

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 alert('Welcome to the Secret Number Game');
-let secretNumber = 29;
-console.log('Secret Number = ' + secretNumber)
+let numberMax = 100;
+let secretNumber = parseInt(Math.random() * numberMax + 1);
+console.log('Secret Number = ' + secretNumber);
 
-let kick = prompt('Choose a number between 1 and 30');
+let kick, attempts = 1;
 
-if (secretNumber == kick) {
-    console.log('Thats right! you discovered the secret number ' + secretNumber);
-} else {
-    console.log('You missed :(');
+while (kick != secretNumber) {
+
+    kick = prompt(`Choose a number between 1 and ${numberMax}`);
+
+    if (secretNumber == kick) {
+        break;
+    } else {
+
+        alert('You missed :(');
+        if (kick > secretNumber) {
+            alert('The secret number is smaller than the ' + kick);
+        } else {
+            alert('The secret number is greater than the ' + kick);
+        }
+        attempts++;
+
+    }
+
 }
+
+let wordAttempt = attempts > 1 ? 'attempts' : 'attempt'
+alert(`Thats right! you discovered the secret number ${secretNumber} with ${attempts} ${wordAttempt}.`)
+
+
